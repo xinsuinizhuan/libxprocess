b731e452e22dfa079003485f4431947bedfd8372
time-killer-games <samuelvenable@hotmail.com>
2021-05-12 23:33:45 -0400

    Add files via upload

--- enigma.h
+++ enigma.h
@@ -25,39 +25,8 @@
  
 */
 
-#define RET_BOOL             bool
-#define RET_VOID             void
-#define ARG_PROCID           PROCID
-#define RET_CONST_CHAR_PTR   void *
-#define ARG_CONST_CHAR_PTR   void *
-#define ARG_INT_OUT          void *
-#define ARG_PROCID_OUT       void *
-#define ARG_PROCID_OUT_VEC   void *
-#define ARG_CHAR_PTR_VEC     void *
-#define ARG_CHAR_OUT_PTR     void *
-#define ARG_CHAR_OUT_PTR_VEC void *
-
 namespace enigma_user {
 
-RET_VOID ProcIdEnumerate(ARG_PROCID_OUT_VEC procId, ARG_INT_OUT size);
-RET_VOID ProcIdFromSelf(ARG_PROCID_OUT procId);
-RET_VOID ParentProcIdFromSelf(ARG_PROCID_OUT parentProcId);
-RET_BOOL ProcIdExists(ARG_PROCID procId);
-RET_BOOL ProcIdKill(ARG_PROCID procId);
-RET_VOID ParentProcIdFromProcId(ARG_PROCID procId, ARG_PROCID_OUT parentProcId);
-RET_VOID ProcIdFromParentProcId(ARG_PROCID parentProcId, ARG_PROCID_OUT_VEC procId, ARG_INT_OUT size);
-RET_VOID ExeFromProcId(ARG_PROCID procId, ARG_CHAR_OUT_PTR buffer);
-RET_CONST_CHAR_PTR DirectoryGetCurrentWorking();
-RET_BOOL DirectorySetCurrentWorking(ARG_CONST_CHAR_PTR dname);
-RET_VOID CwdFromProcId(ARG_PROCID procId, ARG_CHAR_OUT_PTR buffer);
-RET_VOID FreeCmdline(ARG_CHAR_PTR_VEC buffer);
-RET_VOID CmdlineFromProcId(ARG_PROCID procId, ARG_CHAR_OUT_PTR_VEC buffer, ARG_INT_OUT size);
-RET_VOID ParentProcIdFromProcIdSkipSh(ARG_PROCID procId, ARG_PROCID_OUT parentProcId);
-RET_VOID ProcIdFromParentProcIdSkipSh(ARG_PROCID parentProcId, ARG_PROCID_OUT_VEC procId, ARG_INT_OUT size);
-RET_CONST_CHAR_PTR EnvironmentGetVariable(ARG_CONST_CHAR_PTR name);
-RET_BOOL EnvironmentSetVariable(ARG_CONST_CHAR_PTR name, ARG_CONST_CHAR_PTR value);
-RET_VOID FreeEnviron(ARG_CHAR_OUT_PTR buffer);
-RET_VOID EnvironFromProcId(ARG_PROCID procId, ARG_CHAR_OUT_PTR_VEC buffer, ARG_INT_OUT size);
-RET_VOID EnvironFromProcIdEx(ARG_PROCID procId, ARG_CONST_CHAR_PTR name, ARG_CHAR_OUT_PTR value);
+using namespace CrossProcess;
 
 } // namespace enigma_user
