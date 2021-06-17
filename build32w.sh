55268b99feafeae5857c48f045f1906945c517bf
time-killer-games <samuelvenable@hotmail.com>
2021-06-17 11:45:34 -0400

    Add files via upload

--- build32w.sh
+++ build32w.sh
@@ -10,8 +10,9 @@ elif [ $(uname) = "FreeBSD" ]; then
 else
   C:/msys64/msys2_shell.cmd -defterm -mingw32 -no-start -here -lc "g++ crossprocess.cpp -o crossprocess32.exe -std=c++17 -static-libgcc -static-libstdc++ -static -m32";
   C:/msys64/msys2_shell.cmd -defterm -mingw64 -no-start -here -lc "g++ crossprocess.cpp -o crossprocess64.exe -std=c++17 -static-libgcc -static-libstdc++ -static -m64";
-  xxd -i 'crossprocess32' | sed 's/\([0-9a-f]\)$/\0, 0x00/' > 'crossprocess32.h'
-  xxd -i 'crossprocess64' | sed 's/\([0-9a-f]\)$/\0, 0x00/' > 'crossprocess64.h'
-  rm -f "crossprocess32.exe" "crossprocess64.exe"
+  xxd -i 'crossprocess32' | sed 's/\([0-9a-f]\)$/\0, 0x00/' > 'crossprocess32.h';
+  xxd -i 'crossprocess64' | sed 's/\([0-9a-f]\)$/\0, 0x00/' > 'crossprocess64.h';
+  rm -f "crossprocess32.exe" "crossprocess64.exe";
   C:/msys64/msys2_shell.cmd -defterm -mingw32 -no-start -here -lc "g++ gamemaker.cpp crossprocess.cpp -o libxprocess.dll -shared -std=c++17 -static-libgcc -static-libstdc++ -static -lShlwapi -DXPROCESS_WIN32EXE_INCLUDES -DXPROCESS_GUIWINDOW_IMPL -m32";
+  rm -f "crossprocess32.h" "crossprocess64.h";
 fi
