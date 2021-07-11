c1abdf3613d26306c212d7c5c36dc49aaf8c13ed
time-killer-games <samuelvenable@hotmail.com>
2021-07-11 01:28:00 -0400

    Add files via upload

--- enigma.h
+++ enigma.h
@@ -25,208 +25,126 @@
  
 */
 
-#include "crossprocess.h"
+#include <string>
 
-using CrossProcess::PROCID;
-using CrossProcess::PROCESS;
-using CrossProcess::PROCINFO;
-using CrossProcess::PROCLIST;
-#if defined(XPROCESS_GUIWINDOW_IMPL)
-using CrossProcess::WINDOWID;
-#endif
+#include "crossprocess.h"
 
 namespace enigma_user {
 
 // execute process from the shell, return process id
-double ProcessExecute(std::string command) {
-  return CrossProcess::ProcessExecute(command.c_str());
-}
+double ProcessExecute(std::string command);
 
 // execute process from the shell async, return process id
-double ProcessExecuteAsync(std::string command) {
-  return CrossProcess::ProcessExecuteAsync(command.c_str());
-}
+double ProcessExecuteAsync(std::string command);
 
 //  get whether executed process has quit based on process id
-double CompletionStatusFromExecutedProcess(double procIndex) {
-  return CrossProcess::CompletionStatusFromExecutedProcess((PROCESS)procIndex);
-}
+double CompletionStatusFromExecutedProcess(double procIndex);
 
 // write to executed process standard input file descriptor based on process id
-double ExecutedProcessWriteToStandardInput(double procIndex, std::string input) {
-  CrossProcess::ExecutedProcessWriteToStandardInput((PROCESS)procIndex, input.c_str()); return 0;
-}
+double ExecutedProcessWriteToStandardInput(double procIndex, std::string input);
 
 // read from executed process standard output file descriptor based on process id
-std::string ExecutedProcessReadFromStandardOutput(double procIndex) {
-  return CrossProcess::ExecutedProcessReadFromStandardOutput((PROCESS)procIndex);
-}
+std::string ExecutedProcessReadFromStandardOutput(double procIndex);
 
 // free executed process standard input string based on process id
-double FreeExecutedProcessStandardInput(double procIndex) {
-  CrossProcess::FreeExecutedProcessStandardInput((PROCESS)procIndex); return 0;
-}
+double FreeExecutedProcessStandardInput(double procIndex);
 
 // free executed process standard ouptut string based on process id
-double FreeExecutedProcessStandardOutput(double procIndex) {
-  CrossProcess::FreeExecutedProcessStandardOutput((PROCESS)procIndex); return 0;
-}
+double FreeExecutedProcessStandardOutput(double procIndex);
 
 // get process id from self
-double ProcIdFromSelf() {
-  return CrossProcess::ProcIdFromSelf();
-}
+double ProcIdFromSelf();
 
 // get parent process id from self
-double ParentProcIdFromSelf() {
-  return CrossProcess::ParentProcIdFromSelf();
-}
+double ParentProcIdFromSelf();
 
 // get whether process exists based on process id
-double ProcIdExists(double procId) {
-  return CrossProcess::ProcIdExists((PROCID)procId);
-}
+double ProcIdExists(double procId);
 
 // kill process based on process id, return whether succeeded
-double ProcIdKill(double procId) {
-  return CrossProcess::ProcIdKill((PROCID)procId);
-}
+double ProcIdKill(double procId);
 
 // get executable image file path from self
-std::string ExecutableFromSelf() {
-  return CrossProcess::ExecutableFromSelf();
-}
+std::string ExecutableFromSelf();
 
 // get executable image file path from process id
-std::string ExeFromProcId(double procId) {
-  return CrossProcess::ExeFromProcId((PROCID)procId);
-}
+std::string ExeFromProcId(double procId);
 
 // get current working directory from process id
-std::string CwdFromProcId(double procId) {
-  return CrossProcess::CwdFromProcId((PROCID)procId);
-}
+std::string CwdFromProcId(double procId);
 
 // get process info from process id
-double ProcInfoFromProcId(double procId) {
-  return CrossProcess::ProcInfoFromProcId((PROCID)procId);
-}
+double ProcInfoFromProcId(double procId);
 
 // free process info data from memory
-double FreeProcInfo(double procInfo) {
-  CrossProcess::FreeProcInfo((PROCINFO)procInfo); return 0;
-}
+double FreeProcInfo(double procInfo);
 
 // create a list of all process id's
-double ProcListCreate() {
-  return CrossProcess::ProcListCreate();
-}
+double ProcListCreate();
 
 // get process id from process list at index
-double ProcessId(double procList, double i) {
-  return CrossProcess::ProcessId((PROCLIST)procList, (int)i);
-}
+double ProcessId(double procList, double i);
 
 // get amount of process id's in process list
-double ProcessIdLength(double procList) {
-  return CrossProcess::ProcessIdLength((PROCLIST)procList);
-}
+double ProcessIdLength(double procList);
 
 // free list of process id's from memory
-double FreeProcList(double procList) {
-  CrossProcess::FreeProcList((PROCLIST)procList); return 0;
-}
+double FreeProcList(double procList);
 
 // get executable image file path from process info data
-std::string ExecutableImageFilePath(double procInfo) {
-  return CrossProcess::ExecutableImageFilePath((PROCINFO)procInfo);
-}
+std::string ExecutableImageFilePath(double procInfo);
 
 // get current working directory ffrom process info data
-std::string CurrentWorkingDirectory(double procInfo) {
-  return CrossProcess::CurrentWorkingDirectory((PROCINFO)procInfo);
-}
+std::string CurrentWorkingDirectory(double procInfo);
 
 // get parent processs id from process info data
-double ParentProcessId(double procInfo) {
-  return CrossProcess::ParentProcessId((PROCINFO)procInfo);
-}
+double ParentProcessId(double procInfo);
 
 // get child process id from process info data at index
-double ChildProcessId(double procInfo, double i) {
-  return CrossProcess::ChildProcessId((PROCINFO)procInfo, (int)i);
-}
+double ChildProcessId(double procInfo, double i);
 
 // get amount of child processes from process info data
-double ChildProcessIdLength(double procInfo) {
-  return CrossProcess::ChildProcessIdLength((PROCINFO)procInfo);
-}
+double ChildProcessIdLength(double procInfo);
 
 // get command line argument from process info data at index
-std::string CommandLine(double procInfo, double i) {
-  return CrossProcess::CommandLine((PROCINFO)procInfo, (int)i);
-}
+std::string CommandLine(double procInfo, double i);
 
 // get amount of command line arguments from process info data
-double CommandLineLength(double procInfo) {
-  return CrossProcess::CommandLineLength((PROCINFO)procInfo);
-}
+double CommandLineLength(double procInfo);
 
 // get environment variable (NAME=VALUE) from process info at index
-std::string Environment(double procInfo, double i) {
-  return CrossProcess::Environment((PROCINFO)procInfo, (int)i);
-}
+std::string Environment(double procInfo, double i);
 
 // get amount of anvironment variables from process info at index
-double EnvironmentLength(double procInfo) {
-  return CrossProcess::EnvironmentLength((PROCINFO)procInfo);
-}
+double EnvironmentLength(double procInfo);
 
 // get current working directory
-std::string DirectoryGetCurrentWorking() {
-  return CrossProcess::DirectoryGetCurrentWorking();
-}
+std::string DirectoryGetCurrentWorking();
 
 // set current working directory based on a given dname
-double DirectorySetCurrentWorking(std::string dname) {
-  return CrossProcess::DirectorySetCurrentWorking(dname.c_str());
-}
+double DirectorySetCurrentWorking(std::string dname);
 
 // get the environment variable of the given name
-std::string EnvironmentGetVariable(std::string name) {
-  return CrossProcess::EnvironmentGetVariable(name.c_str());
-}
+std::string EnvironmentGetVariable(std::string name);
 
 // set the environment variable with the given name and value
-double EnvironmentSetVariable(std::string name, std::string value) {
-  return CrossProcess::EnvironmentSetVariable(name.c_str(), value.c_str());
-}
+double EnvironmentSetVariable(std::string name, std::string value);
 
 // unset the environment variable with the given name
-double EnvironmentUnsetVariable(std::string name) {
-  return CrossProcess::EnvironmentUnsetVariable(name.c_str());
-}
+double EnvironmentUnsetVariable(std::string name);
 
 #if defined(XPROCESS_GUIWINDOW_IMPL)
 // get owned window id string from process info at index
-std::string OwnedWindowId(double procInfo, double i) {
-  return CrossProcess::OwnedWindowId((PROCINFO)procInfo, (int)i);
-}
+std::string OwnedWindowId(double procInfo, double i);
 
 // get amount of owned window id's from process info at index
-double OwnedWindowIdLength(double procInfo) {
-  return CrossProcess::OwnedWindowIdLength((PROCINFO)procInfo);
-}
+double OwnedWindowIdLength(double procInfo);
 
 // get whether a process exists based on one of its window id's
-double WindowIdExists(std::string winId) {
-  return CrossProcess::WindowIdExists((WINDOWID)winId.c_str());
-}
+double WindowIdExists(std::string winId);
 
 // kill a process based on one of its window id's, return whether succeeded
-double WindowIdKill(std::string winId) {
-  return CrossProcess::WindowIdKill((WINDOWID)winId.c_str());
-}
+double WindowIdKill(std::string winId);
 #endif
 
 } // namespace enigma_user
