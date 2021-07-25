75920bfc90b7fd5ac6f07c56c4b79b8603393798
time-killer-games <samuelvenable@hotmail.com>
2021-07-25 00:31:58 -0400

    Update build64w.sh

--- build64w.sh
+++ build64w.sh
@@ -2,7 +2,7 @@
 cd "${0%/*}"
 
 if [ $(uname) = "Darwin" ]; then
-  clang++ gamemaker.cpp gamemaker.mm crossprocess.cpp -o libxprocess.dylib -shared -std=c++17 -DXPROCESS_GUIWINDOW_IMPL -framework CoreFoundation -framework CoreGraphics -framework Cocoa -ObjC++  -m64;
+  clang++ gamemaker.cpp gamemaker.mm crossprocess.cpp -o libxprocess.dylib -shared -std=c++17 -DXPROCESS_GUIWINDOW_IMPL -framework CoreFoundation -framework CoreGraphics -framework Cocoa -ObjC++ -arch arm64 -arch x86_64;
 elif [ $(uname) = "Linux" ]; then
   g++ gamemaker.cpp crossprocess.cpp -o libxprocess.so -shared -std=c++17 -static-libgcc -static-libstdc++ -lprocps -lpthread -DXPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC -m64;
 elif [ $(uname) = "FreeBSD" ]; then
