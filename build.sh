323b7032f4b37c77a5d6db3204e1f67d3aa0fafa
Samuel Venable <samuelvenable@hotmail.com>
2022-01-12 08:15:31 -0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ elif [ $(uname) = "DragonFly" ]; then
   g++ -c gamemaker.cpp apiprocess/process.cpp -shared -std=c++17 -I/usr/local/include -L/usr/local/lib -static-libgcc -static-libstdc++ -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC && g++ gamemaker.o process.o -o libxprocess.so -shared -std=c++17 -I/usr/local/include -L/usr/local/lib -lkvm -lutil -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC;
   ar rc libxprocess.a gamemaker.o process.o && rm -f "gamemaker.o" "process.o";
 elif [ $(uname) = "OpenBSD" ]; then
-  clang++ -c gamemaker.cpp apiprocess/process.cpp -shared -std=c++17 -I/usr/local/include -L/usr/local/lib -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC && g++ gamemaker.o process.o -o libxprocess.so -shared -std=c++17 -I/usr/local/include -L/usr/local/lib -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC;
+  clang++ -c gamemaker.cpp apiprocess/process.cpp -shared -std=c++17 -I/usr/local/include -L/usr/local/lib -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC && clang++ gamemaker.o process.o -o libxprocess.so -shared -std=c++17 -I/usr/local/include -L/usr/local/lib -lkvm -lc -lpthread -DPROCESS_GUIWINDOW_IMPL `pkg-config x11 --cflags --libs` -fPIC;
   ar rc libxprocess.a gamemaker.o process.o && rm -f "gamemaker.o" "process.o";
 else
   C:/msys64/msys2_shell.cmd -defterm -mingw32 -no-start -here -lc "g++ apiprocess/process.cpp -o process32.exe -std=c++17 -static-libgcc -static-libstdc++ -static -m32";
